46a5fc52dacc703f5d403bee7b30610b49eaab58
Kinfu <ykinfu@deloitte.com>
2019-02-25 16:39:32 -0500

    controller not querying

--- CustomHeadline/force-app/main/default/aura/WelcomePage/WelcomePageController.js
+++ CustomHeadline/force-app/main/default/aura/WelcomePage/WelcomePageController.js
@@ -1,6 +1,6 @@
 ({
     doInit : function(component, event, helper) {
-        var action = component.get("c.finduserInfo");
+        var action = component.get("c.findUserInfo");
         action.setCallback(this, function(response) {
             var state = response.getState();
             if (state === "SUCCESS") {
